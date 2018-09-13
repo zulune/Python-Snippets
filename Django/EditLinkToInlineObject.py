162087c52d34df9c08d0022a12135105cf710069
zulune <mars.xaker@gmail.com>
2018-09-13 16:03:08 +0300

    Edit link to Inline Object

--- Django/EditLinkToInlineObject.py
+++ Django/EditLinkToInlineObject.py
@@ -3,4 +3,20 @@ from django.core.urlresolvers import reverse
 
 
 class EditLinkToInlineObject(object):
-    
+    def init_link(self, instance):
+        url = reverse('admin:%s_%s_change' % (
+            instance._meta.app_label, instance._meta.model_name), 
+            args=[instance.pk])
+        if instance.pk:
+            return mark_safe('<a href="{}">Edit</a>'.format(url))
+        else:
+            return ""
+
+class InstanceModelInline(EditLinkToInlineObject, admin.TabularInline):
+    model = InstanceModel
+    readonly_fields = ('edit_link', )
+
+
+@admin.register(InstanceModel)
+class InstanceModelAdmin(admin.ModelAdmin):
+    inline = (InstanceModelInline, )
